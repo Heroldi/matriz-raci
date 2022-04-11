63474f2a3b96d668cb81ad86fcc6e48b5d8686a0
thiago-augustin <75146536+thiago-augustin@users.noreply.github.com>
2022-04-11 13:11:16 -0300

    Atualização de arquivos de configuração

--- config/config.js
+++ config/config.js
@@ -1,1 +1,29 @@
-// Teste
+//setando para o mode de desenvolvimento
+const env = process.env.NODE_ENV || 'dev';
+
+const config = () => {
+    switch (env) {
+        case 'dev' :
+            return {
+                dbString : 'mongodb+srv://thiago:root123@cluster0.yin34.mongodb.net/Cluster0?retryWrites=true&w=majority',
+                jwtPass : 'senha_web_token',
+                jwtExpires : '5d'
+            }
+        case 'hml' :
+            return {
+                dbString : 'mongodb+srv://thiago:root123@cluster0.yin34.mongodb.net/Cluster0?retryWrites=true&w=majority',
+                jwtPass : 'senha_web_token',
+                jwtExpires : '5d'
+            }
+        case 'prod' :
+            return {
+                dbString : 'mongodb+srv://thiago:root123@cluster0.yin34.mongodb.net/Cluster0?retryWrites=true&w=majority',
+                jwtPass : 'senha_web_token',
+                jwtExpires : '5d'
+            }
+    }
+};
+
+console.log(`Iniciando a API em ambiente ${env.toUpperCase()}`);
+
+module.exports = config();
